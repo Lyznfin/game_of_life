23fddc8860d9022f6fa2c5b09ac5e063285567de
manzil.akbar@gmail.com <manzil.akbar@gmail.com>
2024-04-20 05:31:53 +0700

    Added README.md

---
+++ README.md
@@ -0,0 +1,2 @@
+# Conway's Game of Life
+![](https://github.com/game_of_life/Game_of_life_pulsar.gif)
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from game import Game
 
 def main() -> None:
-    game = Game(40, 80, state_mode="moore", cell_mode="brians")
+    game = Game(soup_path="soups/pulsar.txt", cell_mode="brians")
+    # game = Game(40, 80, state_mode="moore", cell_mode="brians")
     game.start()
 
 if __name__ == "__main__":
