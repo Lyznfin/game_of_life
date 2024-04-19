d4c6f415af176fb5369476f25a8820239527711b
manzil.akbar@gmail.com <manzil.akbar@gmail.com>
2024-04-19 09:32:23 +0700

    Created random_state

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
-def random_state(width: int, height: int, /):
-    pass
+import random
 
-def dead_state(width: int, height: int, /) -> list[list[int]]:
+def random_cell() -> int:
+    random_number = random.random()
+    if random_number >= 0.5:
+        return 1
+    else:
+        return 0
+
+def random_state(height: int, width: int, /):
+    state = dead_state(height, width)
+    for i in range(len(state)):
+        for j in range(len(state[i])): 
+            cell_state = random_cell()
+            state[i][j] = cell_state
+    print(state)
+
+def dead_state(height: int, width: int, /) -> list[list[int]]:
     return [[0 for _ in range(width)] for _ in range(height)]
 
 def main():
-    print(dead_state(5, 5))
+    random_state(5, 5)
 
 if __name__ == "__main__":
     main()
